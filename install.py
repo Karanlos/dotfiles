320ffbf68b07204827824a9928b433f6e1d9ac7e
Erik Sejersen <ehs@dashmodule.dk>
2021-04-03 13:15:31 +0200

    Minor fixes

--- install.py
+++ install.py
@@ -3,24 +3,27 @@
 import os, errno
 from pathlib import Path
 
-home_path = str(Path.home()) 
+home_path = str(Path.home())
 path = home_path + '/.config/'
+current_path = os.path.realpath('./nvim')
 
 def symlink():
-    for root, dirs, files in os.walk('./nvim'):
+    for root, dirs, files in os.walk(os.path.realpath('./nvim')):
         for name in files:
-            target = os.path.realpath(os.path.join(root, name))
-            link_name = os.path.realpath(os.path.join(path, root + '/' + name))
+            target = os.path.relpath(os.path.join(root, name))
+            link_name = os.path.relpath(os.path.join(path, name))
 
-            try:
-                os.symlink(target, link_name)
-            except OSError as error:
-                if error.errno == errno.EEXIST:
-                    os.remove(link_name)
-                    os.symlink(target, link_name)
+#            try:
+#                os.symlink(target, link_name)
+#            except OSError as error:
+#                if error.errno == errno.EEXIST:
+#                    os.remove(link_name)
+#                    os.symlink(target, link_name)
 
+            print(root)
             print(target)
             print(link_name)
+            print(path)
 
 try:
     os.makedirs(path + 'nvim/lua/utils', exist_ok=True)
